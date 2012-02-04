2ba2eae2b563e16ad90695bb4470a8df5f803e16
bryan <bryan@ac7aa084-b6da-11df-bf42-7fac39f0224f>
2012-02-04 09:13:32 +0000

    Add forgotten basicInputOutput.h
    git-svn-id: svn://dutlru0.lr.tudelft.nl/tudat/branches/packages@266 ac7aa084-b6da-11df-bf42-7fac39f0224f

---
+++ Tudat/InputOutput/basicInputOutput.h
@@ -0,0 +1,108 @@
+/*! \file basicInputOutput.h
+ *    Header file that defines the input_output namespace, containing all
+ *    some basic input output functions contained in Tudat.
+ *
+ *    Path              : /InputOutput/
+ *    Version           : 10
+ *    Check status      : Checked
+ *
+ *    Author/Checker    : K. Kumar
+ *    Affiliation       : Delft University of Technology
+ *    E-mail address    : K.Kumar@tudelft.nl
+ *
+ *    Author            : J. Leloux
+ *    Affiliation       : Delft University of Technology
+ *    E-mail address    : J.Leloux@tudelft.nl
+ *
+ *    Checker           : D. Dirkx
+ *    Affiliation       : Delft University of Technology
+ *    E-mail address    : d.dirkx@tudelft.nl
+ *
+ *    Checker           : S. Billemont
+ *    Affiliation       : Delft University of Technology
+ *    E-mail address    : simon@angelcorp.be
+ *
+ *    Date created      : 10 August, 2010
+ *    Last modified     : 27 January, 2011
+ *
+ *    References
+ *
+ *    Notes
+ *
+ *    Copyright (c) 2010-2011 Delft University of Technology.
+ *
+ *    This software is protected by national and international copyright.
+ *    Any unauthorized use, reproduction or modification is unlawful and
+ *    will be prosecuted. Commercial and non-private application of the
+ *    software in any form is strictly prohibited unless otherwise granted
+ *    by the authors.
+ *
+ *    The code is provided without any warranty; without even the implied
+ *    warranty of merchantibility or fitness for a particular purpose.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      100902    K. Kumar          File header and footer added.
+ *      100916    D. Dirkx          Added minor comments and placeholder tag during checking.
+ *      100928    K. Kumar          Added reference and adjusted include statements.
+ *      100929    K. Kumar          Changed EigenRoutines.h include statement
+ *                                  to linearAlgebra.h and removed placeholder comment.
+ *                                  Added small comment modifications.
+ *      110202    K. Kumar          Added overload for map with State* for
+ *                                  computeNearestLeftNeighborUsingBinarySearch().
+ *      110803    J. Leloux         Added convertStringToTemplate.
+ *      110805    J. Leloux         Added outputCurrentRunningTime.
+ *      110810    J. Leloux         Minor comment modifications.
+ *      110913    K. Kumar          Implemented automatic root-path functions based on
+ *                                  suggestions by M. Persson.
+ *      111117    K. Kumar          Added listAllFilesInDirectory() function.
+ *      120127    K. Kumar          Adapted for Tudat Core.
+ */
+
+#ifndef TUDAT_BASICINPUTOUTPUT_H
+#define TUDAT_BASICINPUTOUTPUT_H
+
+// Include statements.
+#include <string>
+
+//! Tudat library namespace.
+/*!
+ * The Tudat Library namespace.
+ */
+namespace tudat
+{
+
+//! The input/output namespace.
+/*!
+ * The input/output namespace.
+ */
+namespace input_output
+{
+
+//! Get root-path for Tudat Package library.
+/*!
+ * Returns root-path corresponding with root-directory of Tudat Core library as a string with
+ * trailing slash included.
+ * \param Root-path.
+ */
+static std::string getPackageRootPath( )
+{
+#ifdef TUDAT_CUSTOM_ROOT_PATH
+    return std::string( TUDAT_CUSTOM_ROOT_PATH );
+#else
+    // Declare file path string assigned to filePath.
+    std::string filePath_( __FILE__ );
+
+    // Strip filename from temporary string and return root-path string.
+    return filePath_.substr( 0, filePath_.length( ) -
+                                std::string( "InputOutput/basicInputOutput.h" ).length( ) );
+#endif
+}
+
+} // namespace input_output
+
+} // namespace tudat
+
+#endif // TUDAT_CORE_BASICINPUTOUTPUT_H
+
+// End of file.
